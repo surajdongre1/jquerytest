625fd17278f17aef024769727483a80ea04840d5
Dongre <sdongre@bmc.com>
2022-04-15 19:05:10 +0530

    added some animatios

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 $(function(){
-    $('.redbox').hide(2232);
-
-});
-$(function(){
-    $('.bluebox').click(function () { 
-    $(this).fadeOut(200)
-    $('.bluebox').fadeIn(2000)
-
+    
+    $('.bluebox2').fadeToggle(1000);
+    $('.bluebox2').fadeToggle(1000);
+    $('.lightbox').delay(1000).fadeTo(1000,0.1,function(){alert("hello")}).delay(2000).animate({opacity:'5'});
+   
+    
+    
+    // $(function(){
+    //     $('.bluebox2').animate({'margin-left':'+=1000px','opacity':'0',
+    //     width:'20px',height:'20',marginTop:'25'},1000);
+    //     $('.bluebox2').animate({'opacity':'100','margin-left':'-=1000px',
+    //     'width':'100px',height:'100px',marginTop:'0'},400)
         
+    $(function(){
+        $('.bluebox2').animate({'margin-left':'+=800px','opacity':'0',
+        width:'20px',height:'20',marginTop:'50'},1000).delay(3000).animate({'opacity':'100',
+        'margin-left':'-=800px',
+        'width':'100px',height:'100px',marginTop:'0'},400)
+        $('p').animate({fontSize:"20px"},1000)
     });
-    $('.greenbox').click(function(){
-        $(this).fadeTo(2222,0.2)
-
-    });
+    
 });
